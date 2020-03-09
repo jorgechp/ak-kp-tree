7dd67ad8c673f896210d3215b98fd969271f511e
jorgechp <jorgechp@correo.ugr.es>
2020-03-09 16:51:57 +0100

    Add EM

--- AbstractPredictor.py
+++ AbstractPredictor.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from random import random
+
 
 class AbstractPredictor(ABC):
     def _load_from_file(self, file_path):
@@ -31,8 +33,39 @@ class AbstractPredictor(ABC):
         self._compute_occurrence_matrix(ak_lines, ak_set, kp_lines, kp_set)
         self._compute_frequency_matrix()
 
+    def compute_non_existing_ak(self, ak_set):
+        #K = num_of_k
+        #len_of_dataset
+
+        pi_list = []
+        mu_list = []
+        sigma_list = []
+        c_list = []
+        is_converged = False
+
+        for i in range(K):
+            pi_list.append(1.0/i)
+            mu_list.append(random.random())
+            sigma_list.append(1)
+
+        while not is_converged:
+            for i in range(len_of_dataset):
+
+
+
+        pi_list = [1.0/k for i in range(K)]
+        mu = [ranfom]
+
+        pass
+
     def compute_kp(self, ak_set, energy = 0.7):
-        kp_scores = self.compute_kp_scores(ak_set)
+        existing_keywords = self._frequency_matrix.columns.intersection(ak_set)
+        non_existing_keywords = self._frequency_matrix.columns.difference(ak_set)
+
+        kp_scores = self.compute_kp_scores(existing_keywords)
+        kp_scores_1 = self.compute_non_existing_ak(non_existing_keywords)
+
+
         if kp_scores is not False:
             kp_scores = kp_scores.sort_values(ascending=False).to_frame(name="score")
             kp_scores = kp_scores[kp_scores['score'] > 0]
